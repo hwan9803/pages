fad97c6defb64e9fbdbd2deaef5f794c762ac68f
Yuhwanbeom <rhb7772002@naver.com>
2026-01-31 01:29:56 +0900

    Fix: 맨처음 페이지 수정

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Footer from './common/Footer'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div style={{
+      diplay: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh'
+    }}>
+      <App style={{
+        flex: '1'
+      }}/>
+      <Footer style={{
+        height: '10%',
+        marginBottom: '0'
+      }}/>
+    </div>
   </React.StrictMode>
 );
 
